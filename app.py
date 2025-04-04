b5fff8e97b196bfcab3b18a77000a90852f594b2
Teoh TeikToe <teohteiktoe@gmail.com>
2025-04-04 01:38:05 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+import time
+import requests
+
+TOKEN = '7669423066:AAF1DXuNqn5WYId4kwFuYmBVePUzwKqHVEI'
+BASE_URL = f'https://api.telegram.org/bot{TOKEN}/'
 
 app = Flask(__name__)
 
@@ -8,8 +13,32 @@ def index():
 
 @app.route('/telegram', methods=['GET','POST'])
 def telegram():
-    
-    return(render_template("telegram.html"))
+    #grab id
+    time.sleep(5)
+    response = requests.get(BASE_URL + 'getUpdates')
+    data = response.json()
+    text = data['result'][-1]['message']['text']
+    chat_id = data['result'][-1]['message']['chat']['id']
+    print("Text:", text)
+    print("Chat ID:", chat_id)
+    send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction, please enter the salary"}'
+    requests.get(send_url)
+    time.sleep(3)
+    response = requests.get(BASE_URL + 'getUpdates')
+    data = response.json()
+    text = data['result'][-1]['message']['text']
+    if text.isnumeric():
+        msg = str(float(text) * 100 + 10)
+        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={msg}'
+        requests.get(send_url)
+    else:
+        msg = "salary must be a number"
+        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={msg}'
+        requests.get(send_url)
+        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction, please enter the salary"}'
+        requests.get(send_url)
+        time.sleep(3)
+    return(render_template("index.html"))
 
 if __name__ == '__main__':
     app.run()
