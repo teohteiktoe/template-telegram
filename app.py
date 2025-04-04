6296254bfca3768d9a653ffe98e29c6933777b63
Teoh TeikToe <teohteiktoe@gmail.com>
2025-04-04 01:20:31 +0000

    1

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route('/telegram', methods=['GET','POST'])
+def telegram():
+    
+    return(render_template("telegram.html"))
+
 if __name__ == '__main__':
     app.run()
